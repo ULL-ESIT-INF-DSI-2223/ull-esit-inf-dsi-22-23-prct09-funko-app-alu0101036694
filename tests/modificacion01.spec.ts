0d54d01915158288b12b9220740d5caa08abf84a
Carla Oval Torres <alu0101036694@ull.edu.es>
2023-03-06 11:19:06 +0000

    Modificación de rational completada

--- tests/modificacion01.spec.ts
+++ tests/modificacion01.spec.ts
@@ -17,23 +17,30 @@ describe("ArithmeticableCollection", () => {
         });
 
         it("should be able to substract items", () => {
-            /*const rationalCollection = new ArithmeticableCollection<Rational>();
+            const rationalCollection = new ArithmeticableCollection<Rational>();
             rationalCollection.addArithmeticable(new Rational(1, 2));
             rationalCollection.addArithmeticable(new Rational(1, 2));
             const result = new Rational(0, 0);
-            //const suma = rationalCollection.getArithmeticable(0).s(rationalCollection.getArithmeticable(1))
-            expect(suma.getValue).to.equal(result.getValue); */
+            const resta = rationalCollection.getArithmeticable(0).subtract(rationalCollection.getArithmeticable(1))
+            expect(resta.getValue).to.equal(result.getValue); 
         });
         
         it("should be able to multiply items", () => {
-            //const item = new Rational(4, 1);
-            //expect(rational.multiply(item)).to.equal(2);
+            const rationalCollection = new ArithmeticableCollection<Rational>();
+            rationalCollection.addArithmeticable(new Rational(1, 2));
+            rationalCollection.addArithmeticable(new Rational(1, 2));
+            const result = new Rational(1, 4);
+            const multiply = rationalCollection.getArithmeticable(0).multiply(rationalCollection.getArithmeticable(1))
+            expect(multiply.getValue).to.equal(result.getValue); 
         });
 
         it("should be able to divide items to the collection", () => {
-            //const item1 = new Rational(4, 1);
-            //const item2 = new Rational(2, 1);
-            //expect(item1.divide(item2)).to.equal(2);
+            const rationalCollection = new ArithmeticableCollection<Rational>();
+            rationalCollection.addArithmeticable(new Rational(2, 1));
+            rationalCollection.addArithmeticable(new Rational(1, 1));
+            const result = new Rational(2, 1);
+            const divide = rationalCollection.getArithmeticable(0).divide(rationalCollection.getArithmeticable(1))
+            expect(divide.getValue).to.equal(result.getValue); 
         });
     });
 
